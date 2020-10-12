4b6c0e69452614d9a6fd3d118120abc3f0d490f5
abdul7oss <61707436+abdul7oss@users.noreply.github.com>
2020-10-13 00:43:01 +0530

    Add files via upload

---
+++ cpp/NodeDepths recursive approach.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class BinaryTree {
+public:
+int value;
+BinaryTree *left;
+BinaryTree *right;
+BinaryTree(int value) {
+this->value = value;
+left = NULL;
+right = NULL;
+}
+};
+
+
+int nodeDepths(BinaryTree *root, int depth);
+
+int nodeDepths(BinaryTree *root, int depth = 0) {
+if (root == NULL)
+return 0;
+return depth + nodeDepths(root->left, depth + 1) +
+nodeDepths(root->right, depth + 1);
+}
+
+int main(){
+
+	BinaryTree *a = new BinaryTree(1);
+	a->left = new BinaryTree(1);
+	a->right = new BinaryTree(5);
+	a->left->left = new BinaryTree(4);
+	a->left->right = new BinaryTree(5);
+	a->right->left = new BinaryTree(6);
+	a->right->right = new BinaryTree(7);
+	a->left->left->left = new BinaryTree(8);
+	a->left->left->right = new BinaryTree(9);
+	
+
+
+
+	int b = nodeDepths(a);
+	cout << b;
+
+	return 0;
+}
